29b6a9de5d690e24bef4968e047ce033da6394cd
zosoftmedia <cyberkhan7@gmail.com>
2020-02-05 22:21:22 +0100

    New file

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import  './Contact.scss'
 import Content from './Content'
@@ -43,50 +42,5 @@ class Contact extends Component{
         )
     }
 }
-=======
-import React, { Component } from 'react'
-import  './Contact.scss'
-import Content from './Content'
-import Radium,{StyleRoot} from 'radium'
-import MainImage from './MainImage'
 
-class Contact extends Component{ 
-    
-    state={
-     
-          Title:"Your Interest is Importent For Us",
-          Address:"Triester Strasse,1100 Wien,Austria",
-          ContantNumber:"+4367761991832",
-          Email:"Office@mypizza.com",
-          fex :"+4367761991832"  
-    }
-    
-    render()
-    {
-        
-        return(
-           
-            <> <StyleRoot>
-                <div className="ContactImageContainer">
-                    <div className="ContactRow">              
-                   
-                   <Content
-                       Title = {this.state.Title}
-                       Address = {this.state.Address}
-                       Phone ={this.state.ContantNumber}
-                       Email = {this.state.Email}
-                       Fex = {this.state.fex}
-                       />
-                   
-
-
-                    <MainImage/>
-                </div>
-                </div> </StyleRoot>
-            </>
-           
-        )
-    }
-}
->>>>>>> Adding Burger
 export default Radium(Contact);
